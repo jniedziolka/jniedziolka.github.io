03a2f1ce147cebf82bfe063d31a12dc4e229796e
jniedziolka <jakub.niedziolka@o2.pl>
2020-11-08 22:09:08 +0100

    Updates

---
+++ precache-manifest.22de79da803bc6406bc49aad74b37372.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7df485e71fba22c63316efdaa75d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443d744c6ee565547b25",
+    "url": "/static/css/main.79f59f5d.chunk.css"
+  },
+  {
+    "revision": "a889e0719b7e965df390",
+    "url": "/static/js/2.e57a3c3d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e57a3c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443d744c6ee565547b25",
+    "url": "/static/js/main.3b5e3c7d.chunk.js"
+  },
+  {
+    "revision": "f9236c57da2ce408de47",
+    "url": "/static/js/runtime-main.3e20b4a1.js"
+  },
+  {
+    "revision": "e2ae6067bb1d3a426d166e27699fb9f9",
+    "url": "/static/media/bookmark.e2ae6067.svg"
+  },
+  {
+    "revision": "20b003e9a225342ffbd9130785693d8e",
+    "url": "/static/media/building.20b003e9.svg"
+  },
+  {
+    "revision": "5fa26d303a2c8c62440b7d76f652eea0",
+    "url": "/static/media/clock.5fa26d30.svg"
+  },
+  {
+    "revision": "6afbc36b5aefa44f9c1b5fe92c3fbefa",
+    "url": "/static/media/email.6afbc36b.svg"
+  },
+  {
+    "revision": "582063ced28bda3474ec6f5edb49e4fc",
+    "url": "/static/media/facebook.582063ce.svg"
+  },
+  {
+    "revision": "66be498c87f8608bf0c58046464996c4",
+    "url": "/static/media/github.66be498c.svg"
+  },
+  {
+    "revision": "1ae84f5943fed7ed1811f6e33ebaf798",
+    "url": "/static/media/home.1ae84f59.svg"
+  },
+  {
+    "revision": "b5acdd67df880137e854e59ee9e56522",
+    "url": "/static/media/html-coding.b5acdd67.svg"
+  },
+  {
+    "revision": "6c773039a065f6c708ef8694bbe0fcd7",
+    "url": "/static/media/idea.6c773039.svg"
+  },
+  {
+    "revision": "3afa2a8a30dfce693b70134f48ffdc0f",
+    "url": "/static/media/like.3afa2a8a.svg"
+  },
+  {
+    "revision": "4dae9ad19023775be93f58e9777411ca",
+    "url": "/static/media/linkedin.4dae9ad1.svg"
+  },
+  {
+    "revision": "4043cbb324394ed56f5c903c1995caa2",
+    "url": "/static/media/phone.4043cbb3.svg"
+  },
+  {
+    "revision": "1cd3edd588779d1f78b17b9e069a0633",
+    "url": "/static/media/speech-bubble.1cd3edd5.svg"
+  },
+  {
+    "revision": "056939fd49a123d98c7ecc0b83cc0ba8",
+    "url": "/static/media/user.056939fd.svg"
+  }
+]);
